90641c0e9f740d835170f5bf03c3723baae525fd
Aaron Nash <aaron27nash@gmail.com>
2023-05-21 00:53:26 +0000

    May 20, 2023, 8:53 PM

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+async function moveNpc(){ 
+await npc. walkEast(2000)
+npc.walkSouth () 
+}
+moveNpc()   
+
+
+    
+  
+  
 
 // Create the inventory
 const inventory = newInventory()
